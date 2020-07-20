6e619d9f959c9488ee154bea6a4dc327434964e2
parkjaehoon <yukihoon12@naver.com>
2020-07-20 23:38:45 +0900

    이벤트 핸들링
    이벤트 핸들링 정리입니다.

---
+++ 이벤트 핸들링/event/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EventPractice from './EventPractice';
+
+function App() {
+  return (
+    <>
+      <EventPractice></EventPractice>
+    </>
+  );
+}
+
+export default App;
---
+++ 이벤트 핸들링/event/src/EventPractice.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+
+const EventPractice = ()=>{
+    const [form, setForm] = useState({username:'', message:''});
+    const{username, message} = form;
+    const onChange = e=>{
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
+    }
+    // const [username, setUsername] = useState('');
+    // const [message, setMessage] = useState('');
+    // const onChangeUsername = e => setUsername(e.target.vlaue);
+    // const onChangeMessage = e => setMessage({[e.target.name]: e.target.value});
+    const onClick = () => {
+    alert(message+ ':' +username);
+        setForm({
+            username:'',
+            message:''
+        });
+    };
+    const onKeyPress = e =>{
+        if(e.key === 'Enter'){
+            onClick();
+        }
+    }
+    return(
+    <div>
+        <h1>이벤트 연습</h1>
+        <input
+            type="text"
+            name="message"
+            placeholder="아무거나입력해요"
+            value={message}
+            onChange={onChange}
+            onKeyPress={onKeyPress}
+        />
+        <input
+            type="text"
+            name="username"
+            placeholder="사용자명"
+            value={username}
+            onChange={onChange}>
+        </input>
+        <button onClick={onClick}>
+            정답
+        </button>
+    </div>
+    )
+}
+
+// class EventPractice extends Component{
+//     state = {
+//         message:'',
+//         username:''
+//     }
+//     handleChange = (e)=>{
+//         this.setState({
+//             [e.target.name]: e.target.value
+//         });
+//     }
+//     handleClick = (e)=>{
+//         alert(this.state.message+ ':' +this.state.username);
+//         this.setState({
+//             message:'',
+//             username:''
+//         })
+//     }
+//     handleKeyPress = (e) =>{
+//         if(e.key === 'Enter'){
+//             this.handleClick();
+//         }
+//     }
+//         render(){
+//         return (
+//             <div>
+//                 <h1>이벤트 연습</h1>
+//                 <input
+//                 type="text"
+//                 name="message"
+//                 placeholder="아무거나입력해요"
+//                 value={this.state.message}
+//                 onChange={this.handleChange}
+//                 onKeyPress={this.handleKeyPress}
+//                 />
+//                 <input
+//                 type="text"
+//                 name="username"
+//                 placeholder="사용자명"
+//                 value={this.state.username}
+//                 onChange={this.handleChange}>
+//                 </input>
+//                 <button onClick={this.handleClick}>
+//                     정답
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+
+export default EventPractice;

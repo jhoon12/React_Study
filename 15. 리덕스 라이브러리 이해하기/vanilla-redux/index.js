0824b96b62773a16431d41501e75494a1e403fe8
parkjaehoon <yukihoon12@naver.com>
2020-09-13 23:43:54 +0900

    redux(1.5/2)

--- 15. 리덕스 라이브러리 이해하기/vanilla-redux/index.js
+++ 15. 리덕스 라이브러리 이해하기/vanilla-redux/index.js
@@ -1,14 +1,72 @@
-console.log("hello parcel");
+import { createStore } from "redux";
 
-const divToggle = document.querySelector('.toggle');
-const counter = document.querySelector('h1');
-const btnIncrease = document.querySelector('#increase');
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
 const btnDecrease = document.querySelector("#decrease");
 
 const TOGGLE_SWITCH = "TOGGlE_SWITCH";
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 
-const toggleSwitch = () => ({type=TOGGLE_SWITCH});
-const increase = () =>({type=INCREASE, differ});
-const decresae = ()=>({type=DECREASE});
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decresae = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH: {
+      return {
+        ...state, //불변성 유지
+        toggle: !state.toggle,
+      };
+    }
+    case INCREASE: {
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    }
+    case DECREASE: {
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState(); //현재 상태를 불러옴
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+
+  counter.innerText = state.counter;
+};
+render();
+
+store.subscribe(render);//액션이 디스패치 되어 상태가 업데이트 될 때마다 호출
+
+
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+  store.dispatch(decresae());
+};
